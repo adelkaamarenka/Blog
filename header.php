20653446f0eb6c694836fa782e02bbfff24689e5
adelkaamarenka <adelka.dilaj@gmail.com>
2022-03-07 00:30:55 +0100

    changed home link

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
       <div class="header-content">
         <h1 class="logo">TestWebsite</h1>
         <nav>
-          <a href="#" class="link home">Home</a>
+          <a href="./index.php" class="link home">Home</a>
           <a href="#" class="link about">About us</a>
           <div class="log-in button">
             <a href="./login.php" class="log-in-text"><p>Log in</p></a>
