06ee8649498d3568b15d0a4e8bf7fd3de677ba7e
adelkaamarenka <adelka.dilaj@gmail.com>
2022-03-06 19:13:43 +0100

    changed index content

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Test Website</title>
+    <link rel="stylesheet" href="./styles/index.css" />
+  </head>
+  <body>
+    <header>
+      <div class="header-content">
+        <h1 class="logo">TestWebsite</h1>
+        <nav>
+          <a href="#" class="link home">Home</a>
+          <a href="#" class="link about">About us</a>
+          <div class="log-in button">
+            <a href="./login.php" class="log-in-text"><p>Log in</p></a>
+          </div>
+          <div class="sign-up button">
+            <a href="./register.php" class="sign-up-text"><p>Sign up</p></a>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <div class="website-content">
+      <div class="main-content"></div>
+    </div>
+  </body>
+</html>
