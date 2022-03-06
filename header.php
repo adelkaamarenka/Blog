1ce7f79d395da25198bf2bf98e305384a042ba84
adelkaamarenka <adelka.dilaj@gmail.com>
2022-03-06 19:15:49 +0100

    moved the header contents to a seperate file

---
+++ header.php
@@ -0,0 +1,15 @@
+    <header>
+      <div class="header-content">
+        <h1 class="logo">TestWebsite</h1>
+        <nav>
+          <a href="#" class="link home">Home</a>
+          <a href="#" class="link about">About us</a>
+          <div class="log-in button">
+            <a href="./login.php" class="log-in-text"><p>Log in</p></a>
+          </div>
+          <div class="sign-up button">
+            <a href="./register.php" class="sign-up-text"><p>Sign up</p></a>
+          </div>
+        </nav>
+      </div>
+    </header>
--- index.php
+++ index.php
@@ -8,21 +8,9 @@
     <link rel="stylesheet" href="./styles/index.css" />
   </head>
   <body>
-    <header>
-      <div class="header-content">
-        <h1 class="logo">TestWebsite</h1>
-        <nav>
-          <a href="#" class="link home">Home</a>
-          <a href="#" class="link about">About us</a>
-          <div class="log-in button">
-            <a href="./login.php" class="log-in-text"><p>Log in</p></a>
-          </div>
-          <div class="sign-up button">
-            <a href="./register.php" class="sign-up-text"><p>Sign up</p></a>
-          </div>
-        </nav>
-      </div>
-    </header>
+    <?php
+      include_once 'header.php'
+      ?>
     <div class="website-content">
       <div class="main-content"></div>
     </div>
