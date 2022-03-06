120eeed9bb620d355ad141d38f37bda02d1b81e4
adelkaamarenka <adelka.dilaj@gmail.com>
2022-03-06 19:40:38 +0100

    obsolete div removed

--- register.php
+++ register.php
@@ -24,7 +24,7 @@
         </form>
         <div class="error"></div>
         <div class="register"><a href="./registered.php"><p>Register</p></a></div>
-    </div></div>
+    </div>
     
 </body>
 </html>
