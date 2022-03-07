dae2de74374b1a01bcdccadb7908ec3b1f537832
adelkaamarenka <adelka.dilaj@gmail.com>
2022-03-07 15:35:18 +0100

    changed the syntax and added more error handlers

--- include/register.inc.php
+++ include/register.inc.php
@@ -1,25 +1,32 @@
 <?php
 
-if(isset($_POST["submit"])) {
-    $email = $_POST["email"];
-    $pwd = $_POST["password"];
-    $pwdrepeat = $_POST["rep-password"];
-
-
-    include "../classes/signup-cont-classes.php";
-    include "../classes/signup-classes.php";
-
-    $signup = new SignupCont($email, $pwd, $pwdrepeat);
-
-
-    if ($signup->emptyInput()) {
-        echo "error";
-    }
-
-    if ($signup->invalidEmail()) {
-        echo "error";
-    }
-}
-else {
+if (!isset($_POST["submit"])) {
     header("location: ../register.php");
+}
+
+$email = $_POST["email"];
+$pwd = $_POST["password"];
+$pwdrepeat = $_POST["rep-password"];
+
+
+include "../classes/signup-cont-classes.php";
+include "../classes/signup-classes.php";
+
+$signup = new SignupCont($email, $pwd, $pwdrepeat);
+
+
+if ($signup->emptyInput()) {
+    echo "empty input";
+}
+
+if ($signup->invalidEmail()) {
+    echo "invalid email";
+}
+
+if ($signup->invalidPassword()) {
+    echo "invalid password";
+}
+
+if ($signup->passwordsDontMatch()) {
+    echo "passwords dont match";
 }
