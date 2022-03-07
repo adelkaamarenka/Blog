7f718840c61c187b806bb1fddf4c3b481a32ad30
adelkaamarenka <adelka.dilaj@gmail.com>
2022-03-07 22:26:11 +0100

    added comments

--- classes/dbh.classes.php
+++ classes/dbh.classes.php
@@ -8,11 +8,12 @@ class Dbh {
 
     public $con = null;
 
+    //establishes a connection to the database
     public function __construct() {
-        $this->con = msqli_connect($this->host, $this->username, $this->password, $this->dbname);
-        if(this->con->connect_error) {
+        $this->con = mysqli_connect($this->host, $this->username, $this->password, $this->dbname);
+        if($this->con->connect_error) {
             echo "Fail ".$this->con->connect_error;
         }
-        echo "Connection successful";
+        echo nl2br("Connection successful \n");
     }
 }
