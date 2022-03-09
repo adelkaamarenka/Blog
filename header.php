6b300254088607281eec8af3a33c62c1d91b1532
adelkaamarenka <adelka.dilaj@gmail.com>
2022-03-09 01:14:08 +0100

    changed name

--- header.php
+++ header.php
@@ -2,10 +2,10 @@
     
     <header>
       <div class="header-content">
-        <h1 class="logo">TestWebsite</h1>
+        <h1 class="logo">Kocham dupy</h1>
         <nav>
           <a href="./index.php" class="link home">Home</a>
-          <a href="#" class="link about">About us</a>
+          <a href="#" class="link about">Zobacz memy</a>
           <div class="log-in button">
             <a href="./login.php" class="log-in-text"><p>Log in</p></a>
           </div>
