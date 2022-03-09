b3ac28f01c06910973fd2064e675eeaf1560dfef
adelkaamarenka <adelka.dilaj@gmail.com>
2022-03-09 01:14:25 +0100

    initialized the session and changed the content

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Test Website</title>
+    <title>Kocham dupy</title>
     <link rel="stylesheet" href="./styles/index.css" />
   </head>
   <body>
@@ -12,7 +16,16 @@
       include_once 'header.php'
       ?>
     <div class="wrapper">
-      <div class="main-content"></div>
+      <div class="main-content">
+        <?php
+          if (!isset($_SESSION["id"])) {
+            echo 'You are not logged in';
+          }
+          else {
+            echo 'You are logged in';
+          }
+        ?>
+      </div>
     </div>
   </body>
 </html>
